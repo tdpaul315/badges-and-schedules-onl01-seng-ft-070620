50f1c50e789d90d61658c3e47a02dc09a811c490
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-14 04:13:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(badge_maker)
+  badge_maker = []
+  badge_maker.each do |badge_maker|
+ end
+ return badge_maker(name)
+end
