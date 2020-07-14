b3e00f880817e39421f8aa046225a45d9de13ede
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-14 04:21:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(batch)
   new_batch = []
   batch.each do |name|
-    new_batch.push("Hello, my name is #{name}".)
+    new_batch.push ("Hello, my name is #{name}".)
   end
   return new_batch
 end 
